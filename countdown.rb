449575e6cbc7f5bfdd7af8b2b14bd1213f80f5c9
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-18 19:06:43 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,23 +1,23 @@
 #write your code here
 
-def countdown(number) 
-count = number 
+def countdown(number)
+count = number
 
-  if number > 0 
-  puts "#{number} SECOND(S)!" 
-    number -= 1 
-    end 
-  
-  return "HAPPY NEW YEAR!" 
-  end 
-  
- 
-def countdown_with_sleep(number) 
-  count = number 
- if number > 0 
-  puts "#{number} SECOND(S)!" 
-    sleep(1) 
-    number -= 1 
-    end 
- return "HAPPY NEW YEAR!" 
+  if number > 0
+  puts "#{number} SECOND(S)!"
+    number -= 1
+    end
+
+  return "HAPPY NEW YEAR!"
+  end
+
+
+def countdown_with_sleep(number)
+  count = number
+ if number > 0
+  puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+    end
+ return "HAPPY NEW YEAR!"
 end 
